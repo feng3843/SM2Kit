84b99aed6709e9fca42229ebf1b78f8864938b24
zhuxinhua <feng3843@qq.com>
2021-03-31 10:25:02 +0800

    Initial Commit

---
+++ XHSM2Kit/XHSM2Kit.h
@@ -0,0 +1,19 @@
+//
+//  XHSM2Kit.h
+//  XHSM2Kit
+//
+//  Created by 朱鑫华 on 2021/3/31.
+//  Copyright © 2021 朱鑫华. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XHSM2Kit.
+FOUNDATION_EXPORT double XHSM2KitVersionNumber;
+
+//! Project version string for XHSM2Kit.
+FOUNDATION_EXPORT const unsigned char XHSM2KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XHSM2Kit/PublicHeader.h>
+
+
---
+++ XHSM2KitTests/XHSM2KitTests.m
@@ -0,0 +1,37 @@
+//
+//  XHSM2KitTests.m
+//  XHSM2KitTests
+//
+//  Created by 朱鑫华 on 2021/3/31.
+//  Copyright © 2021 朱鑫华. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XHSM2KitTests : XCTestCase
+
+@end
+
+@implementation XHSM2KitTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
